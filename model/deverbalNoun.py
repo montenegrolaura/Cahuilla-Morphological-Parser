d3b3f003f5a3e37ec64e66ea2e8306a68c2d88fd
Laura Montenegro <lauramontenegro@arizona.edu>
2024-08-07 12:59:18 -0700

    update class and tests to output full results

--- model/deverbalNoun.py
+++ model/deverbalNoun.py
@@ -11,7 +11,10 @@ class DeverbalNoun:
         self.nominalizer_gloss = nominalizer_gloss
         self.class1relationship = class1relationship
 
-        def __str__(self):
-            return(f"The definition is '{self.definition}'"
-                   f"The root verb is '{self.root}' which means '{self.root_gloss}'. This verb is {self.transitivity}."
-                   f"The nominalizer is '{self.nominalizer}' which is a {self.nominalizer_gloss} and means '{self.class1Relationship}'")
+    def __str__(self):
+        transitivity_str = self.transitivity.name.lower().replace('_', ' ') if self.transitivity else ''
+        class1relationship_str = self.class1relationship.name.lower().replace('_', ' ') if self.transitivity else ''
+
+        return(f"The definition is '{self.definition}'. "
+               f"The root verb is '{self.root}' which means '{self.root_gloss}'. This verb is {transitivity_str}. "
+               f"The nominalizer is '{self.nominalizer}' which is a {self.nominalizer_gloss} and means '{class1relationship_str}'.")
--- test/parser_tests.py
+++ test/parser_tests.py
@@ -19,7 +19,15 @@ def services():
 def test_parser_1(services):
     word_surface = 'vúvanpiš'
     components = services.parse_word(word_surface)
+    print(str(components))
 
+    expected_str = (
+        "The definition is 'an insect that stings'. "
+        "The root verb is 'vúvan' which means 'to hit'. This verb is transitive. "
+        "The nominalizer is 'piš' which is a NMLZ.FUT and means 'event not yet occurred'."
+    )
+
+    assert str(components) == expected_str
     assert components.definition == 'an insect that stings'
     assert components.root == 'vúvan'
     assert components.root_gloss == 'to hit'
@@ -32,6 +40,13 @@ def test_parser_1(services):
 def test_parser_2(services):
     word_surface = 'ʔáminat'
     components = services.parse_word(word_surface)
+    print(str(components))
+
+    expected_str = (
+        "The definition is 'the throwing or the orphan'. "
+        "The root verb is 'ʔámin' which means 'to throw'. This verb is transitive. "
+        "The nominalizer is 'at' which is a NMLZ.ABST and means 'verbal abstract noun'."
+    )
 
     assert components.definition == 'the throwing or the orphan'
     assert components.root == 'ʔámin'
@@ -45,6 +60,13 @@ def test_parser_2(services):
 def test_parser_3(services):
     word_surface = 'kʷáʔisniat'
     components = services.parse_word(word_surface)
+    print(str(components))
+
+    expected_str = (
+        "The definition is 'the writing'. "
+        "The root verb is 'kʷáʔisni' which means 'to write'. This verb is transitive. "
+        "The nominalizer is 'at' which is a NMLZ.ABSTT and means 'verbal abstract noun'."
+    )
 
     assert components.definition == 'the writing'
     assert components.root == 'kʷáʔisni'
@@ -58,6 +80,13 @@ def test_parser_3(services):
 def test_parser_4(services):
     word_surface = 'pívat'
     components = services.parse_word(word_surface)
+    print(str(components))
+
+    expected_str = (
+        "The definition is 'tobacco'. "
+        "The root verb is 'pívaʔ' which means 'to smoke'. This verb is intransitive. "
+        "The nominalizer is 'at' which is a NMLZ.ABST and means 'verbal abstract noun'."
+    )
 
     assert components.definition == 'tobacco'
     assert components.root == 'pívaʔ'
@@ -71,6 +100,13 @@ def test_parser_4(services):
 def test_parser_5(services):
     word_surface = 'kʷáʔisniʔil̃'
     components = services.parse_word(word_surface)
+    print(str(components))
+
+    expected_str = (
+        "The definition is 'the writing'. "
+        "The root verb is 'kʷáʔisni' which means 'to write'. This verb is transitive. "
+        "The nominalizer is 'ʔil̃' which is a NMLZ.ABST and means 'verbal abstract noun'."
+    )
 
     assert components.definition == 'the writing'
     assert components.root == 'kʷáʔisni'
@@ -84,6 +120,13 @@ def test_parser_5(services):
 def test_parser_6(services):
     word_surface = 'kúyil̃'
     components = services.parse_word(word_surface)
+    print(str(components))
+
+    expected_str = (
+        "The definition is 'the burial'. "
+        "The root verb is 'kúy' which means 'to bury'. This verb is transitive. "
+        "The nominalizer is 'il̃' which is a NMLZ.ABST and means 'verbal abstract noun'."
+    )
 
     assert components.definition == 'the burial'
     assert components.root == 'kúy'
@@ -97,6 +140,13 @@ def test_parser_6(services):
 def test_parser_7(services):
     word_surface = 'ʔásniʔil̃'
     components = services.parse_word(word_surface)
+    print(str(components))
+
+    expected_str = (
+        "The definition is 'the bathing, the bath'. "
+        "The root verb is 'ʔásni' which means 'to bathe'. This verb is transitive. "
+        "The nominalizer is 'ʔil̃' which is a NMLZ.ABST and means 'verbal abstract noun'."
+    )
 
     assert components.definition == 'the bathing, the bath'
     assert components.root == 'ʔásni'
@@ -110,6 +160,13 @@ def test_parser_7(services):
 def test_parser_8(services):
     word_surface = 'kúpvel'
     components = services.parse_word(word_surface)
+    print(str(components))
+
+    expected_str = (
+        "The definition is 'the bed'. "
+        "The root verb is 'kúp' which means 'to sleep'. This verb is intransitive. "
+        "The nominalizer is 'vel' which is a NMLZ.PFV and means 'event not yet occurred'."
+    )
 
     assert components.definition == 'the bed'
     assert components.root == 'kúp'
@@ -123,6 +180,13 @@ def test_parser_8(services):
 def test_parser_9(services):
     word_surface = 'yúmuvel'
     components = services.parse_word(word_surface)
+    print(str(components))
+
+    expected_str = (
+        "The definition is 'the hat'. "
+        "The root verb is 'yúmu' which means 'to put on the head'. This verb is transitive. "
+        "The nominalizer is 'vel' which is a NMLZ.PFV and means 'already occurring or occurred'."
+    )
 
     assert components.definition == 'the hat'
     assert components.root == 'yúmu'
@@ -136,19 +200,33 @@ def test_parser_9(services):
 def test_parser_10(services):
     word_surface = 'qáwivel'
     components = services.parse_word(word_surface)
+    print(str(components))
+
+    expected_str = (
+        "The definition is 'handle'. "
+        "The root verb is 'qáwi' which means 'to get tied'. This verb is transitive. "
+        "The nominalizer is 'vel' which is a NMLZ.PFV and means 'event already occurring or occurred'."
+    )
 
     assert components.definition == 'handle'
     assert components.root == 'qáwi'
     assert components.root_gloss == 'to get tied'
     assert components.transitivity == Transitivity.TRANSITIVE
-    assert components.class1relationship == Class1Relationship.VERBAL_ABSTRACT_NOUN
+    assert components.class1relationship == Class1Relationship.ALREADY_OCCURRING_OR_OCCURRED
     assert components.nominalizer == 'vel'
     assert components.nominalizer_gloss == 'NMLZ.PFV'
 
 
-def test_parser_10(services):
+def test_parser_11(services):
     word_surface = 'čeŋenval'
     components = services.parse_word(word_surface)
+    print(str(components))
+
+    expected_str = (
+        "The definition is 'dancing place'. "
+        "The root verb is 'čeŋen' which means 'to dance'. This verb is transitive. "
+        "The nominalizer is 'val' which is a NMLZ.LOC and means 'location or place'."
+    )
 
     assert components.definition == 'dancing place'
     assert components.root == 'čeŋen'
